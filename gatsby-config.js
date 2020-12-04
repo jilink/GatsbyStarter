358e5a4cad4b4f425223cf9415d6f8ef3a07e179
jilink <jimmyfac1@gmail.com>
2020-12-04 12:16:20 +0100

    Working panda blog with MD pages automatically creating pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Pandas eating LOTS`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Contact = () => {
+  return (
+    <Layout>
+      <p>Contact us bro</p>
+    </Layout>
+  )
+}
+
+export default Contact
